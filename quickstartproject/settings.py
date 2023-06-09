6e0e49e1f5a5dd2b3a5868e040217b23817d69e9
Paula <106445181+primzahlenpaula@users.noreply.github.com>
2023-06-09 13:57:52 +0200

    fix django frozen_importlib

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'create_ticket'
+    'create_ticket',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
