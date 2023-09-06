105193ba3c8428b4d2c83e21d2bd7decc9777bd3
nicolescherliess <132930684+nicolescherliess@users.noreply.github.com>
2023-09-06 21:51:27 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -1,24 +1,21 @@
-const dropdown = document.getElementById('dynamicDropdown');
+window.onload = function () {
+    const dropdown = document.getElementById('dynamicDropdown');
 
-async function populateDropdown() {
-    try {
-        if (!dropdown) {
-            console.error('Das Dropdown-Element mit der ID "dynamicDropdown" wurde nicht gefunden.');
-            return;
+    async function populateDropdown() {
+        try {
+            const response = await fetch('https://iu-isef01-functionapp.azurewebsites.net/api/GetCourses?');
+            const data = await response.json();
+
+            data.forEach(option => {
+                const optionElement = document.createElement('option');
+                optionElement.value = option.id;
+                optionElement.textContent = option.shortname;
+                dropdown.appendChild(optionElement);
+            });
+        } catch (error) {
+            console.error('Fehler beim Befüllen der Dropdown-Liste:', error);
         }
-
-        const response = await fetch('https://iu-isef01-functionapp.azurewebsites.net/api/GetCourses?');
-        const data = await response.json();
-        
-        data.forEach(option => {
-            const optionElement = document.createElement('option');
-            optionElement.value = option.id; 
-            optionElement.textContent = option.shortname; 
-            dropdown.appendChild(optionElement);
-        });
-    } catch (error) {
-        console.error('Fehler beim Befüllen der Dropdown-Liste:', error);
     }
-}
 
-populateDropdown();
+    populateDropdown();
+};
