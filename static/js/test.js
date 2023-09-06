9e0b4c6a28f8cf527bee7c4ef8c2d43d8d892c96
nicolescherliess <132930684+nicolescherliess@users.noreply.github.com>
2023-09-06 21:41:34 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -2,6 +2,11 @@ const dropdown = document.getElementById('dynamicDropdown');
 
 async function populateDropdown() {
     try {
+        if (!dropdown) {
+            console.error('Das Dropdown-Element mit der ID "dynamicDropdown" wurde nicht gefunden.');
+            return;
+        }
+
         const response = await fetch('https://iu-isef01-functionapp.azurewebsites.net/api/GetCourses?');
         const data = await response.json();
         
@@ -9,10 +14,11 @@ async function populateDropdown() {
             const optionElement = document.createElement('option');
             optionElement.value = option.id; 
             optionElement.textContent = option.shortname; 
-            dropdown.appendChild(option);
+            dropdown.appendChild(optionElement);
         });
     } catch (error) {
         console.error('Fehler beim Befüllen der Dropdown-Liste:', error);
     }
 }
+
 populateDropdown();
