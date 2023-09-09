aafa638a3c35301fc5992d810649996d00ff0562
nicolescherliess <132930684+nicolescherliess@users.noreply.github.com>
2023-09-09 12:59:35 +0200

    Select2-Dropdown

--- hello_azure/Seiten/DropdownCourse.js
+++ hello_azure/Seiten/DropdownCourse.js
@@ -6,12 +6,16 @@ window.onload = function () {
             const response = await fetch('https://iu-isef01-functionapp.azurewebsites.net/api/GetCourses?');
             const data = await response.json();
 
+            // Füge die Daten zum Dropdown-Feld hinzu
             data.forEach(option => {
                 const optionElement = document.createElement('option');
                 optionElement.value = option.id;
                 optionElement.textContent = option.shortname;
                 dropdown.appendChild(optionElement);
             });
+
+            // Initialisiere das Dropdown-Feld mit Select2
+            $(dropdown).select2();
         } catch (error) {
             console.error('Fehler beim Befüllen der Dropdown-Liste:', error);
         }
