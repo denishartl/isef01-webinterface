27d95a27aa97509cf038ae2cd0852269a2a4f82c
Denis Hartl <denis.hartl@icloud.com>
2023-09-30 17:51:50 +0200

    Fix imports

--- tickets/views.py
+++ tickets/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import requests
-import json
 
 def index(request):
     print('Request for index page received')
